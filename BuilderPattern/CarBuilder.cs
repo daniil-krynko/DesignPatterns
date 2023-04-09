b43d3f26115b10caa075cca1f341e38c2a0f067b
Daniil Krynko <daniil.krynko@gmail.com>
2023-04-09 18:04:08 +0300

    Created classes "Car" and its builder "CarBuilder"

---
+++ BuilderPattern/CarBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class CarBuilder : IBuilder
+{
+	Car car;
+	uint seats;
+	string engine;
+	bool tripComputer;
+	bool gps;
+
+    public CarBuilder()
+	{
+		this.Reset();
+	}
+	public void Reset()
+	{ this.car = new Car(); }
+    public void SetSeats(uint count)
+	{ seats = count; }
+    public void SetEngine(string engine)
+	{ this.engine = engine; }
+    public void SetTripComputer(bool computer)
+	{ tripComputer = computer; }
+    public void SetGPS(bool gps)
+	{ this.gps = gps; }
+    public Car GetProduct()
+	{
+		Car product = this.car;
+		this.Reset();
+		return product;
+	}
+}
