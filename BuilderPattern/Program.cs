a0e8b3611aef01aee443d4de7b2dda2c73ce9b7b
Daniil Krynko <daniil.krynko@gmail.com>
2023-04-09 18:04:57 +0300

    Filled "main" method

--- BuilderPattern/Program.cs
+++ BuilderPattern/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace BuilderPattern
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuilderPattern
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Example of Builder pattern");
+            Console.WriteLine("Example of Builder pattern\n");
+
+            Director director = new Director();
+
+            CarBuilder builder = new CarBuilder();
+            director.ConstructSportCar(builder);
+            Car car = builder.GetProduct();
         }
     }
 }
