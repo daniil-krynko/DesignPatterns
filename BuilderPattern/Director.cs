a8c86e90929c7682697edbc4551196c9b8173f0f
Daniil Krynko <daniil.krynko@gmail.com>
2023-04-09 18:04:26 +0300

    Created director class "Director"

---
+++ BuilderPattern/Director.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Director
+{
+    public Director()
+    {
+
+    }
+    public void ConstructSportCar(IBuilder builder)
+    {
+        builder.Reset();
+        builder.SetSeats(2);
+        builder.SetEngine("twin-turbocharged Tipo F120A");
+        builder.SetTripComputer(true);
+        builder.SetGPS(true);
+    }
+}
