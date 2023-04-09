5e1e8d50ffc859eba3e3fe30fb10154ad297b320
Daniil Krynko <daniil.krynko@gmail.com>
2023-04-09 21:54:47 +0300

    Done "main" method

---
+++ AbstractFactoryPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿#define MacOS
+
+namespace AbstractFactoryPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Example of Abstract Factory pattern\n");
+#if MacOS
+            MacFactory factory = new MacFactory();
+#else
+            WinFactory factory = new WinFactory();
+#endif
+            GUI ui = new GUI(factory);
+            ui.CreateUI();
+            ui.Paint();
+        }
+    }
+}
