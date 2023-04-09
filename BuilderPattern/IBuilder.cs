cc8b0b134f7c1617d2e4752cd2acee9af7aaed9f
Daniil Krynko <daniil.krynko@gmail.com>
2023-04-09 18:03:41 +0300

    Created interface "IBuilder"

---
+++ BuilderPattern/IBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public interface IBuilder
+{
+    void Reset();
+    void SetSeats(uint seats);
+    void SetEngine(string engine);
+    void SetTripComputer(bool tripComputer);
+    void SetGPS(bool gps);
+}
