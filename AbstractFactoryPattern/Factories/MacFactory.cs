8d9b62942fa4a2d7a57ceb3d4970dbbd458fd2fe
Daniil Krynko <daniil.krynko@gmail.com>
2023-04-09 21:54:34 +0300

    Created factory classes

---
+++ AbstractFactoryPattern/Factories/MacFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class MacFactory : IGUIFactory
+{
+	public MacFactory()
+	{ }
+    public IButton CreateButton()
+    {
+        Console.WriteLine("Creating Mac button");
+        return new MacButton();
+    }
+    public ICheckbox CreateCheckbox()
+    {
+        Console.WriteLine("Creating Mac checkbox");
+        return new MacCheckbox();
+    }
+}
---
+++ AbstractFactoryPattern/Factories/WinFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class WinFactory : IGUIFactory
+{
+	public WinFactory()
+	{ }
+	public IButton CreateButton()
+	{
+        Console.WriteLine("Creating Windows button");
+        return new WinButton();
+	}
+	public ICheckbox CreateCheckbox()
+	{
+        Console.WriteLine("Creating Windows checkbox");
+        return new WinCheckbox();
+	}
+}
